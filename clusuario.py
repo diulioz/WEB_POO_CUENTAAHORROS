81c7da568954e23148df48c8281a6b66b02658a1
diulioz <diulioz.u@gmail.com>
2022-12-14 10:07:30 -0500

    inicio

---
+++ clusuario.py
@@ -0,0 +1,8 @@
+class usuario:
+    #nombre, apellido, cédula, edad) y método
+#constructor.
+    def __init__(self, nombre, apellido, cedula, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.cedula = cedula
+        self.edad = edad
